3f54b9ffccff96704912a88f2a5ffaf7cd5d307c
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2016-02-11 22:42:38 -0600

    Setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+
+
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(stream: TcpStream) {
+    // ...
+    println!("Correct request received!");
+}
+
 fn main() {
-    println!("Hello, world!");
+	let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+	// accept connections and process them, spawning a new thread for each one
+	for stream in listener.incoming() {
+	    match stream {
+	        Ok(stream) => {
+	            thread::spawn(move|| {
+	                // connection succeeded
+	                handle_client(stream)
+	            });
+	        }
+	        Err(e) => {
+	        	println!("{}", e);
+	        }
+	    }
+	}
+
+	// close the socket server
+	drop(listener);
 }
